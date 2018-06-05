8c201e3d05cd71c78729c9028eeaa6182a5e4b58
Alex Rozhavsky <alexr@ceshbel.co.il>
2018-06-05 14:05:43 +0300

    syntax modifications

--- content.js
+++ content.js
@@ -1,53 +1,47 @@
-InboxSDK.load('1', 'sdk_prext_240120584f').then(function(sdk){
+InboxSDK.load('1', 'sdk_prext_240120584f').then(sdk => {
 	// the SDK has been loaded, now do something with it!
 	function formatDate(date) {
-	  var monthNames = [
+	  const monthNames = [
 	    "January", "February", "March",
 	    "April", "May", "June", "July",
 	    "August", "September", "October",
 	    "November", "December"
 	  ];
 
-	  var day = date.getDate();
-	  var monthIndex = date.getMonth();
-	  var year = date.getFullYear();
+	  const day = date.getDate();
+	  const monthIndex = date.getMonth();
+	  const year = date.getFullYear();
 
 	  return day + ' ' + monthNames[monthIndex] + ' ' + year;
 	}
 
-	sdk.Compose.registerComposeViewHandler(function(composeView){
-		// Confirmed, Draft
-
-		// a compose view has come into existence, do something with it!
+	sdk.Compose.registerComposeViewHandler(composeView => {
 		composeView.addButton({
 			title: "Priority",
 			iconUrl: 'https://is4-ssl.mzstatic.com/image/thumb/Purple122/v4/fe/39/f2/fe39f262-f2f6-61f7-5734-8bc1796ca028/source/60x60bb.jpg',
 			onClick: function(event) {
 				const currentText = document.querySelector('.Am').textContent;
-				var re = /(.*:)(.*)(:.*)/;
-				var status = currentText.replace(re, "$2");
+				const re = /(.*:)(.*)(:.*)/;
+				const status = currentText.replace(re, "$2"); // Confirmed, Draft
 
 				$.ajax({
 				    type: "GET",
 				    url: "https://www.eshbelsaas.com/ui/odata/Priority/tabmob.ini/usdemo/ORDERS?$filter=ORDSTATUSDES eq '" + status +"'",
 				    cache : false,
-				    // jsonp : "onJSONPLoad",
-				    // jsonpCallback: "callback",
 				    contentType: "application/json",
 				    crossDomain: "true",
 				    username: "apidemo",
 				    password: "123",
-			        beforeSend: function (xhr) { 
+			        beforeSend: xhr => { 
         				xhr.setRequestHeader('Authorization', 'Basic ' + btoa("apidemo:123"));             
     				},
-				    success: function (data) {
+				    success: data => {
         				let ordersList = '';
-        				data.value.forEach((order) => {			
-        					ordersList = ordersList + "Order Name: " + order.ORDNAME + '\n';
-        					ordersList = ordersList + "Customer Name: " + order.CDES + '\n';    					
-        					ordersList = ordersList + "Order Date: " + formatDate(new Date(order.CURDATE)) + '\n';
-    						ordersList = ordersList + "Order Status: " + order.ORDSTATUSDES + '\n\n';
-
+        				data.value.forEach(order => {	
+        					ordersList = `${ordersList} Order Name: ${order.ORDNAME} \n`;
+        					ordersList = `${ordersList} Customer Name: ${order.CDES} \n`;
+        					ordersList = `${ordersList} Order Date: ${formatDate(new Date(order.CURDATE))} \n`;
+        					ordersList = `${ordersList} Order Status ${order.ORDSTATUSDES} \n\n`;
         				})
 
         				event.composeView.insertTextIntoBodyAtCursor('\n\n' + ordersList);
